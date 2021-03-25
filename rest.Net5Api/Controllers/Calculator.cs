db9a1e88b7f3544f32a2a1b43c4e88b42542a4ef
UEIDE SANTOS <usjunior@gmail.com>
2021-03-25 18:55:09 -0300

    .

---
+++ rest.Net5Api/Controllers/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rest.Net5Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Calculator : ControllerBase
+    {
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public ActionResult sum(string firstNumber, string secondNumber)
+        {
+            if (Isnumeric(firstNumber) && Isnumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Bad Input");
+        }
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue = 0;
+            if (decimal.TryParse(strNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        private bool Isnumeric(string strNumber)
+        {
+            double number;
+            return double.TryParse(
+                strNumber, 
+                System.Globalization.NumberStyles.Any, 
+                System.Globalization.NumberFormatInfo.InvariantInfo, 
+                out number);
+        }
+    }
+}
